3dfad8128d94a6787e8bea5344f0d2ead33f9dec
Amit <amitsamant@gmail.com>
2026-01-03 13:52:15 +0530

    some more

--- linked-list/reverse_linkedlist.py
+++ linked-list/reverse_linkedlist.py
@@ -1,5 +1,5 @@
 def reverseLinkedList(head):
-    # assign p1 as null and p2 as the first node, fpr p3 we will assign in the while loop
+    # assign p1 as null and p2 as the first node, for p3 we will assign in the while loop
     p1, p2 = None, head
     while p2 is not None:
         p3 = p2.next # first assign the next node to p3 - so we have all variables assigned
@@ -7,5 +7,5 @@ def reverseLinkedList(head):
 
         #shifting forward
         p1 = p2 #now we are shifting forward so p1 moves to p2
-        p2 = p3 # we are shifting forward sp p2 moves to p3
+        p2 = p3 # we are shifting forward so p2 moves to p3
     return p1
