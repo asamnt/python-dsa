3df67067c318b334d535eea8109da2e711e53f02
Amit <37043202+asamnt@users.noreply.github.com>
2026-02-01 10:37:34 +0530

    Optimize trapping rain water calculation
    Refactor water trapping algorithm to improve efficiency by using left_max and right_max arrays.

--- array/trapping_rain_water.py
+++ array/trapping_rain_water.py
@@ -1,7 +1,7 @@
 # # Naive
 # we loop through each element
 # for each element , we find the max bar to its left, and max bar to it right
-# the difference between these two is the amount of water that will get trapped, 
+# the minimum between these two is the amount of water that will get trapped, 
 # BUT reduce the amount with the element at the current index
 # because it is a bar of that height there, so no water gets trapped
 
@@ -22,6 +22,35 @@ def trap_water(arr):
     #now the result is 
     result = result + (right_max - left_max) - arr[i]
 
+#Efficient
+def trap_water(arr):
+  result = 0
+  # create a new array that has the left_max for each element , and create another array that has right_max for each element
+  # now when you iterate, you aleady know the left max for the preevious element, so when you know it, compare that to the current element
+  # and if the current element is greater then update the left max for the element, other the previous elements's left max is also the 
+# current the currrent elements left max
+  l_max = [0] * len(arr)
+  r_max = [0] * len(arr)
+
+  for i in range(1, n): # go from element 1 to last element
+    if arr[i] > l_max[i-1]:
+      l_max[i] = arr[i]
+    else:
+      l_max[i] = l_max[i-1]
+  r_max[n-1] = arr[n-1]
+  for i in range(n-1, -1, -1): #go reverse from second last element to first element
+    r_max[i] = max(r_max[i+1] , arr[])
+
+  #
+  for i in range(1, n-1):
+    result = result + min(l_max[i], r_max[]) - arr[i]
+  return result
+    
+
+
+  
+
     
   
     
+
