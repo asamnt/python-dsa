b309614114cd10b06582ff6a8624f0949fe6a1a0
Amit <37043202+asamnt@users.noreply.github.com>
2026-02-01 09:06:47 +0530

    Enhance remove_duplicate function with comments
    Added comments to clarify logic for removing duplicates.

--- array/easy/remove_duplicate.py
+++ array/easy/remove_duplicate.py
@@ -1,7 +1,9 @@
 def remove_duplicate(arr):
     res = 1
     for i in range(1, len(arr)):
-        if arr[res-1] != arr[i]:
-            arr[res] = arr[i]
+        if arr[res-1] != arr[i]: #if the current element is not the same as previous element indicated by res index
+            arr[res] = arr[i] # then copy over the element from i to where res is 
             res += 1
     return res
+
+# if the element at res index and i is same, we skip it, that is move ahead
